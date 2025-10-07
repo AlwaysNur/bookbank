426c0c043a6af2b3f8849844244f5209bc8680c5
alwaysnur <171459295+AlwaysNur@users.noreply.github.com>
2025-10-07 13:58:02 +1300

    feat(isbn-helper): get description function

--- helper/isbn/isbn.go
+++ helper/isbn/isbn.go
@@ -23,5 +23,31 @@ func GetCoverUrlByIsbn(isbn string) string {
 	if err != nil {
 		log.Println("Error reading response body")
 	}
-	return gjson.Get(string(body), "items.0.volumeInfo.imageLinks.thumbnail").String()
+	coverImage := gjson.Get(string(body), "items.0.volumeInfo.imageLinks.thumbnail").String()
+	if coverImage == "" {
+		log.Println("An unknowen error occurred")
+		return "/static/image/placeholder.png"
+	}
+	return coverImage
+}
+
+func GetDescriptionByIsbn(isbn string) string {
+	if isbn == "" {
+		return "No description provided (No ISBN provided)"
+	}
+	resp, err := http.Get(fmt.Sprintf("https://www.googleapis.com/books/v1/volumes?q=isbn:%v", strings.ReplaceAll(isbn, "-", "")))
+	if err != nil {
+		log.Println("Error sending GET request")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error reading response body")
+	}
+	description := gjson.Get(string(body), "items.0.volumeInfo.description").String()
+	if description == "" {
+		log.Println("An unknowen error occurred")
+		return "No description provided"
+	}
+	return description
 }
