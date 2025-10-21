1afaf12dcd2c6a691bd9d82055588c0336554899
alwaysnur <171459295+AlwaysNur@users.noreply.github.com>
2025-10-22 12:48:52 +1300

    wip: book actions

--- web/static/js/listen.js
+++ web/static/js/listen.js
@@ -1,8 +1,19 @@
 const audio = document.querySelector('audio'); // <audio>
 const descElement = document.querySelector(".description")
 const savedPosition = localStorage.getItem(`bookbankPlaybackLocation-${id}`);
-
+const toolbarOptions = document.getElementById("toolbar-options")
+const toolBarDiv = document.getElementById("toolbar-options-div")
 descElement.innerHTML = descElement.innerHTML.replaceAll(".", ".<br>").replaceAll("!", "!<br>")
+
+function handleMoreClick() {
+    if (toolBarDiv.style.display == "none") {
+        toolBarDiv.style.display = "block";
+        toolBarDiv.style.opacity = "1";
+    } else {
+        toolBarDiv.style.display = "none";
+        toolBarDiv.style.opacity = "0";
+    }
+}
 
 if (savedPosition) {
     audio.currentTime = parseFloat(savedPosition);
@@ -15,4 +26,5 @@ window.addEventListener('beforeunload', () => {
 audio.addEventListener('pause', () => {
     localStorage.setItem(`bookbankPlaybackLocation-${id}`, audio.currentTime);
 });
+
 
